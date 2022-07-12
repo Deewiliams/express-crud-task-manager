f08865c28e60c337297e59f3a7c9301dc1669147
Desire <desireirankundawilliams@gmail.com>
2022-07-13 01:23:18 +0200

    added middleware route

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,15 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 const dotenv = require("dotenv");
 dotenv.config();
+const notFound = require('./middleware/not-found')
 
 // middleware
 app.use(express.json())
 
 //routes
 app.use('/api/v1/tasks',tasks)
+
+app.use(notFound)
 
 const port = 3000; 
 

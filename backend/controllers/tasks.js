92a0f3093446ac400e0940c8c871f2548ad0ded8
Desire <desireirankundawilliams@gmail.com>
2022-07-12 23:31:48 +0200

    able to get a single task by id

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,4 +1,6 @@
 const Task = require('../models/Tasks') 
+
+
 const getAllTasks =  async(req,res) => {
     try {
         const tasks = await Task.find({})
@@ -18,8 +20,14 @@ const createTask = async (req,res) => {
    
 }
 
-const getTask = (req,res) => {
-    res.send('get single tasks')
+const getTask =async (req,res) => {
+    try {
+        const {id:taskID} = req.params;
+        const task = await Task.findOne({_id:taskID});
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error}) 
+    }
 }
 
 const updateTask = (req,res) => {

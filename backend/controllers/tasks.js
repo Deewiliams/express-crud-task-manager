fc41823d2458a377ef01517ec38d5a5871c75b3f
Desire <desireirankundawilliams@gmail.com>
2022-07-12 23:46:16 +0200

    able to delete by id

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -24,6 +24,9 @@ const getTask =async (req,res) => {
     try {
         const {id:taskID} = req.params;
         const task = await Task.findOne({_id:taskID});
+        if(!task){
+            return res.status(404).json({mes:`No task with id: ${taskID}`})
+        }
         res.status(200).json({task})
     } catch (error) {
         res.status(500).json({msg: error}) 
@@ -34,8 +37,17 @@ const updateTask = (req,res) => {
     res.send('update tasks')
 }
 
-const deleteTask = (req,res) => {
-    res.send('delete tasks')
+const deleteTask = async(req,res) => { 
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.deleteOne({_id: taskID })
+        if(!task){
+            return res.status(404).json({mes:`No task with id: ${taskID}`})
+        }
+        res.status(200).json({task})
+    } catch (error) {
+        res.status(500).json({msg: error}) 
+    }
 }
 module.exports = {
     getAllTasks,
@@ -43,6 +55,4 @@ module.exports = {
     getTask,
     updateTask,
     deleteTask
-
-
 }

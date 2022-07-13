6982fb266372b57c724120dd4aafce80f0d0651b
Desire <desireirankundawilliams@gmail.com>
2022-07-13 02:36:38 +0200

    added error file to handle status code errors

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,6 +1,6 @@
 const Task = require('../models/Tasks') 
 const asyncWrapper = require('../middleware/async')
-
+const {createCustomerError} = require('../errors/custom-error')
 
 const getAllTasks = asyncWrapper(async(req,res) => {
         const tasks = await Task.find({})
@@ -16,31 +16,32 @@ const getTask = asyncWrapper(async (req,res, next) => {
         const {id:taskID} = req.params;
         const task = await Task.findOne({_id:taskID});
         if(!task){
-            const error = new Error('Not Found')
-            error.status = 404
-            return next(error)
+            // const error = new Error('Not Found')
+            // error.status = 404
+            // return next(error)
             // return res.status(404).json({mes:`No task with id: ${taskID}`})
+            return next(createCustomerError(`No task with id: ${taskID}`, 404))
         }
         res.status(200).json({task})
 })
 
-const updateTask = asyncWrapper(async (req,res) => {
+const updateTask = asyncWrapper(async (req,res,next) => {
         const {id:taskID} = req.params;
         const task = await fineOneAndUpdate({_id: taskID}, req.body, {
             new: true,
             runValidators: true,
         })
         if(!task){
-            return res.status(404).json({mes:`No task with id: ${taskID}`})
+            return next(createCustomerError(`No task with id: ${taskID}`, 404))
         }
         res.status(200).json({task})
 })
 
-const deleteTask = asyncWrapper(async(req,res) => { 
+const deleteTask = asyncWrapper(async(req,res, next) => { 
         const {id:taskID} = req.params
         const task = await Task.deleteOne({_id: taskID })
         if(!task){
-            return res.status(404).json({mes:`No task with id: ${taskID}`})
+            return next(createCustomerError(`No task with id: ${taskID}`, 404))
         }
         res.status(200).json({task})
 })
